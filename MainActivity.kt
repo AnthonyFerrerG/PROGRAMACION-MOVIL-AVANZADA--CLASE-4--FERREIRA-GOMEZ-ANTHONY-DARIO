7ad6e4dbd474ba8c1cd18d66250a695c27190132
AnthonyFerrerG <anthonydario999710@gmail.com>
2025-09-29 18:45:05 -0300

    MainActivity.kt

--- MainActivity.kt
+++ MainActivity.kt
@@ -3,3 +3,16 @@ fun mostrarTexto(view: View) {
     val tvResultado = findViewById<TextView>(R.id.tvResultado)
     tvResultado.text = etTexto.text.toString()
 }
+
+override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+
+    val etTexto = findViewById<EditText>(R.id.etTexto)
+    val btnConfirmar = findViewById<Button>(R.id.btnConfirmar)
+    val tvResultado = findViewById<TextView>(R.id.tvResultado)
+
+    btnConfirmar.setOnClickListener {
+        tvResultado.text = etTexto.text.toString()
+    }
+}
